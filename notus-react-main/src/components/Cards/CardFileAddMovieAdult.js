3e5f9a202a8b5a795a23cc29152e4270a56645df
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2022-02-22 16:22:12 -0600

    act

--- notus-react-main/src/components/Cards/CardFileAddMovieAdult.js
+++ notus-react-main/src/components/Cards/CardFileAddMovieAdult.js
@@ -8,19 +8,20 @@ export default function CardFileAddMovieAdult() {
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
                 <div className="px-6">
                     <div className="flex flex-wrap justify-center">
-                        <div className="w-full px-4 flex justify-center">
+                        <div className="w-full px-4 flex justify-center items-center p-16 content-center">
                             <div className="relative">
                                 <img
                                     alt="..."
-                                    src={require("assets/img/team-2-800x800.jpg").default}
-                                    className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+                                    src={require("assets/img/back.jpg").default}
+                                    className="obsolute rounded-3xl -m-16 -ml-20 lg:-ml-16"
                                 />
                                 <img
                                     alt="..."
-                                    src={require("assets/img/team-2-800x800.jpg").default}
-                                    className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-12 -ml-20 lg:-ml-16 max-w-150-px"
+                                    src={require("assets/img/poster.jpg").default}
+                                    className="shadow-xl border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
                                 />
                             </div>
+                            
                         </div>
                         <div className="w-full px-4 text-center mt-20">
                             <div className="flex justify-center py-4 lg:pt-4 pt-8">
@@ -29,7 +30,7 @@ export default function CardFileAddMovieAdult() {
                         </div>
                     </div>
                     <div className="text-center mt-12">
-                        <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+                        <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 ">
                             Jennasssss Stones
                         </h3>
                         <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">

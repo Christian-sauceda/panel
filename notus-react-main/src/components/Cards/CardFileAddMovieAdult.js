e8381268fa10fdd12d1c6c5842911db36cf96505
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2022-02-25 15:58:48 -0600

    act

--- notus-react-main/src/components/Cards/CardFileAddMovieAdult.js
+++ notus-react-main/src/components/Cards/CardFileAddMovieAdult.js
@@ -1,36 +1,27 @@
 import React from "react";
-
+import "./card.css";
 // components
 
 export default function CardFileAddMovieAdult() {
     return (
         <>
-            <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
-                <div className="px-6">
-                    <div className="flex flex-wrap justify-center">
-                        <div className="w-full px-4 flex justify-center items-center p-16 content-center">
-                            <div className="relative">
-                                <img
-                                    alt="..."
-                                    src={require("assets/img/back.jpg").default}
-                                    className="obsolute rounded-3xl -m-16 -ml-20 lg:-ml-16"
-                                />
-                                <img
-                                    alt="..."
-                                    src={require("assets/img/poster.jpg").default}
-                                    className="shadow-xl border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
-                                />
-                            </div>
-
-                        </div>
-                        <div className="w-full px-4 text-center mt-20">
-                            <div className="flex justify-center py-4 lg:pt-4 pt-8">
-
-                            </div>
-                        </div>
+            <div className=" flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
+                <div>
+                    <div className="relative left0 top0">
+                        <img
+                            className=''
+                            alt="..."
+                            src={require("assets/img/back.jpg").default}
+                        />
+                        <img
+                            alt="..."
+                            src={require("assets/img/poster.jpg").default}
+                            className="eye absolute" />
                     </div>
-                    <div className="text-center mt-12">
-                        <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 ">
+
+
+                    <div className="text-center md:mt-10 mt-20">
+                        <h3 className="text-xl font-semibold leading-normal mb-2  text-blueGray-700 ">
                             Adjuntar Portada:
                         </h3>
                         <div className="mb-2 text-blueGray-600 mt-10">
--- notus-react-main/src/index.js
+++ notus-react-main/src/index.js
@@ -6,12 +6,10 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 
 // layouts
-
 import Admin from "layouts/Admin.js";
 import Auth from "layouts/Auth.js";
 
 // views without layouts
-
 import Login from "views/auth/Login.js";
 
 ReactDOM.render(

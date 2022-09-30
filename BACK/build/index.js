cdcc0a272ed158324c565fe0ee33f15f5103a6fc
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2022-09-30 11:22:03 -0600

    delete

--- BACK/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-_app["default"].listen(3001);
-
-console.log('Server is running on http://localhost:3001');

77d395cbfc6a1f5cf677124cc133eed3a7f4a0a5
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2023-05-01 14:07:58 -0600

    act

--- BACK/src/controllers/downloadimage.controllers/image.controllers.js
+++ BACK/src/controllers/downloadimage.controllers/image.controllers.js
@@ -9,15 +9,40 @@ const imageController = {
         const type = req.params.type;
         const image = req.params.image;
         const pathImage = path.resolve(__dirname, `../../../images/imgs/${content}/${type}/${image}`);
-        //imgs\moviesadult\back\MOVIE XXXback.jpg
-        if (await fs.existsSync(pathImage)) {
+
+        // Verificar si la imagen existe
+        if (fs.existsSync(pathImage)) {
+            // Eliminar la imagen existente
+            try {
+                fs.unlinkSync(pathImage);
+            } catch (error) {
+                console.error('Error deleting existing image:', error);
+                // Puedes manejar el error de eliminación de imagen de alguna manera específica
+                // por ejemplo, enviar una respuesta de error al cliente
+                return res.status(500).send('Internal Server Error');
+            }
+        }
+
+        // Crear el directorio si no existe
+        if (!fs.existsSync(pathImage)) {
+            try {
+                fs.mkdirSync(path.dirname(pathImage), { recursive: true });
+            } catch (error) {
+                console.error('Error creating directory:', error);
+                // Puedes manejar el error de creación de directorio de alguna manera específica
+                // por ejemplo, enviar una respuesta de error al cliente
+                return res.status(500).send('Internal Server Error');
+            }
+        }
+
+        if (fs.existsSync(pathImage)) {
             res.sendFile(pathImage);
         } else {
             const pathNoImage = path.resolve(__dirname, `../../../images/no-image.png`);
             res.sendFile(pathNoImage);
         }
     }
-}
+};
 
 export default imageController;
 
